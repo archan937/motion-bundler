5151a583166126c732c5ce406d95e56dc49ac4f1
Paul Engel <paul.engel@holder.nl>
2013-04-21 02:20:27 +0200

    Changed SimpleCov.coverage_dir to "test/coverage"

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,6 +3,7 @@ $:.unshift File.expand_path("../../lib", __FILE__)
 
 if Dir.pwd == File.expand_path("../..", __FILE__)
   require "simplecov"
+  SimpleCov.coverage_dir "test/coverage"
   SimpleCov.start
 end
 
