4e5ea5284cd3706dca5d5e659fd2c8bc55860a89
Paul Engel <paul.engel@holder.nl>
2013-05-03 01:13:09 +0200

    Cleaned up test_tracer.rb a bit

--- test/unit/require/test_tracer.rb
+++ test/unit/require/test_tracer.rb
@@ -7,46 +7,29 @@ module Unit
       describe MotionBundler::Require::Tracer do
 
         describe "calling `start`" do
-          before do
+          it "should create a log instance" do
             Thread.current[:motion_bundler_log] = nil
             MotionBundler::Require::Tracer.start
-          end
-
-          it "should create a log instance" do
             assert_equal MotionBundler::Require::Tracer::Log, Thread.current[:motion_bundler_log].class
             assert_equal MotionBundler::Require::Tracer .log, Thread.current[:motion_bundler_log]
           end
 
           describe "having the log instance already defined" do
             it "should clear the log instance" do
+              MotionBundler::Require::Tracer.start
               MotionBundler::Require::Tracer.log.expects(:clear)
               MotionBundler::Require::Tracer.start
             end
           end
 
           it "should hook into `Kernel#require` and `Object#require`" do
-            assert_equal true, Kernel.respond_to?(:require_with_hook)
-            assert_equal true, Object.respond_to?(:require_with_hook)
-
             MotionBundler::Require::Tracer::Hooks.expects :hook
             MotionBundler::Require::Tracer.start
           end
         end
 
         describe "calling `stop`" do
-          before do
-            MotionBundler::Require::Tracer.start
-          end
-
           it "should unhook from `Kernel#require` and `Object#require`" do
-            assert_equal true, Kernel.respond_to?(:require_with_hook)
-            assert_equal true, Object.respond_to?(:require_with_hook)
-
-            MotionBundler::Require::Tracer.stop
-
-            assert_equal false, Kernel.respond_to?(:require_with_hook)
-            assert_equal false, Object.respond_to?(:require_with_hook)
-
             MotionBundler::Require::Tracer::Hooks.expects :unhook
             MotionBundler::Require::Tracer.stop
           end
