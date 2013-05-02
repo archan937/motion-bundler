349a27a3c6c05bbf1b1bc07e3de7a0456dc1bcb1
Paul Engel <paul.engel@holder.nl>
2013-05-03 01:16:25 +0200

    Hooking into `require_relative`, `load` and `autoload` for tracing files and files dependencies

--- lib/motion-bundler/require/tracer/hooks.rb
+++ lib/motion-bundler/require/tracer/hooks.rb
@@ -5,22 +5,32 @@ module MotionBundler
         extend self
 
         def hook
-          Kernel.instance_eval &require_hook unless Kernel.respond_to?(:require_with_hook)
-          Object.class_eval    &require_hook unless Object.respond_to?(:require_with_hook)
+          Kernel.instance_eval &require_hook          unless Kernel.respond_to?(:require_with_hook)
+          Object.class_eval    &require_hook          unless Object.respond_to?(:require_with_hook)
+          Kernel.instance_eval &require_relative_hook unless Kernel.respond_to?(:require_relative_with_hook)
+          Object.class_eval    &require_relative_hook unless Object.respond_to?(:require_relative_with_hook)
+          Kernel.instance_eval &load_hook             unless Kernel.respond_to?(:load_with_hook)
+          Object.class_eval    &load_hook             unless Object.respond_to?(:load_with_hook)
+          Module.class_eval    &autoload_hook         unless Module.respond_to?(:autoload_with_hook)
         end
 
         def unhook
-          Kernel.instance_eval &require_unhook if Kernel.respond_to?(:require_with_hook)
-          Object.class_eval    &require_unhook if Object.respond_to?(:require_with_hook)
+          Kernel.instance_eval &require_unhook          if Kernel.respond_to?(:require_with_hook)
+          Object.class_eval    &require_unhook          if Object.respond_to?(:require_with_hook)
+          Kernel.instance_eval &require_relative_unhook if Kernel.respond_to?(:require_relative_with_hook)
+          Object.class_eval    &require_relative_unhook if Object.respond_to?(:require_relative_with_hook)
+          Kernel.instance_eval &load_unhook             if Kernel.respond_to?(:load_with_hook)
+          Object.class_eval    &load_unhook             if Object.respond_to?(:load_with_hook)
+          Module.class_eval    &autoload_unhook         if Module.respond_to?(:autoload_with_hook)
         end
 
       private
 
         def require_hook
           @require_hook ||= proc do
-            def require_with_hook(path)
+            def require_with_hook(path, _caller = nil)
               result = nil
-              MotionBundler::Require::Tracer.log.register caller[0] do
+              MotionBundler::Require::Tracer.log.register(_caller || caller[0]) do
                 result = require_without_hook path
               end
               result
@@ -30,11 +40,66 @@ module MotionBundler
           end
         end
 
+        def require_relative_hook
+          @require_relative_hook ||= proc do
+            def require_relative_with_hook(path)
+              caller[0].match(/^(.*\.rb)\b/)
+              require_with_hook File.expand_path(path, $1), caller[0]
+            end
+            alias :require_relative_without_hook :require_relative
+            alias :require_relative :require_relative_with_hook
+          end
+        end
+
+        def load_hook
+          @load_hook ||= proc do
+            def load_with_hook(path)
+              require_with_hook path, caller[0]
+            end
+            alias :load_without_hook :load
+            alias :load :load_with_hook
+          end
+        end
+
+        def autoload_hook
+          @autoload_hook ||= proc do
+            def autoload_with_hook(mod, filename)
+              require_with_hook filename, caller[0]
+            end
+            alias :autoload_without_hook :autoload
+            alias :autoload :autoload_with_hook
+          end
+        end
+
         def require_unhook
           @require_unhook ||= proc do
             alias :require :require_without_hook
             undef :require_with_hook
             undef :require_without_hook
+          end
+        end
+
+        def require_relative_unhook
+          @require_relative_unhook ||= proc do
+            alias :require_relative :require_relative_without_hook
+            undef :require_relative_with_hook
+            undef :require_relative_without_hook
+          end
+        end
+
+        def load_unhook
+          @load_unhook ||= proc do
+            alias :load :load_without_hook
+            undef :load_with_hook
+            undef :load_without_hook
+          end
+        end
+
+        def autoload_unhook
+          @autoload_unhook ||= proc do
+            alias :autoload :autoload_without_hook
+            undef :autoload_with_hook
+            undef :autoload_without_hook
           end
         end
 
--- test/unit/require/tracer/test_hooks.rb
+++ test/unit/require/tracer/test_hooks.rb
@@ -9,14 +9,40 @@ module Unit
           it "should hook into core methods" do
             assert_equal false, Kernel.respond_to?(:require_with_hook)
             assert_equal false, Object.respond_to?(:require_with_hook)
+            assert_equal false, Kernel.respond_to?(:require_relative_with_hook)
+            assert_equal false, Object.respond_to?(:require_relative_with_hook)
+            assert_equal false, Kernel.respond_to?(:load_with_hook)
+            assert_equal false, Object.respond_to?(:load_with_hook)
 
             MotionBundler::Require::Tracer::Hooks.send :hook
             assert_equal true, Kernel.respond_to?(:require_with_hook)
             assert_equal true, Object.respond_to?(:require_with_hook)
+            assert_equal true, Kernel.respond_to?(:require_relative_with_hook)
+            assert_equal true, Object.respond_to?(:require_relative_with_hook)
+            assert_equal true, Kernel.respond_to?(:load_with_hook)
+            assert_equal true, Object.respond_to?(:load_with_hook)
+
+            MotionBundler::Require::Tracer.log.expects(:register).with("#{__FILE__}:#{__LINE__ + 1}:in `block (2 levels) in <class:TestHooks>'")
+            require "a"
+
+            MotionBundler::Require::Tracer.log.expects(:register).with("#{__FILE__}:#{__LINE__ + 1}:in `block (2 levels) in <class:TestHooks>'")
+            require_relative "../../../../lib/b"
+
+            MotionBundler::Require::Tracer.log.expects(:register).with("#{__FILE__}:#{__LINE__ + 1}:in `block (2 levels) in <class:TestHooks>'")
+            load "c.rb"
+
+            MotionBundler::Require::Tracer.log.expects(:register).with("#{__FILE__}:#{__LINE__ + 2}:in `<module:SlotMachine>'")
+            module ::SlotMachine
+              autoload :VERSION, gem_path("slot_machine/lib/slot_machine/version")
+            end
 
             MotionBundler::Require::Tracer::Hooks.send :unhook
             assert_equal false, Kernel.respond_to?(:require_with_hook)
             assert_equal false, Object.respond_to?(:require_with_hook)
+            assert_equal false, Kernel.respond_to?(:require_relative_with_hook)
+            assert_equal false, Object.respond_to?(:require_relative_with_hook)
+            assert_equal false, Kernel.respond_to?(:load_with_hook)
+            assert_equal false, Object.respond_to?(:load_with_hook)
           end
         end
 
